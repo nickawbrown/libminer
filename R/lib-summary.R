3be4dd0d263aa1d137bd2e33329bdeb8b7b11568
Nick Brown <nickawbrown@gmail.com>
2024-11-04 12:02:53 -0800

    add documentation for lib_summary()

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,3 +1,13 @@
+#' Provide number of R packages by library
+#'
+#' Provide the number of R packages by library in a data frame
+#'
+#' @return a data.frame of R packages by library
+#'
+#' @export
+#'
+#' @examples
+#' lib_summary()
 lib_summary <- function() {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
